029a3abb04190157f95aa3842d67c58a4204f1d9
Sujin Jeon <mackerel0106@gmail.com>
2024-01-20 02:08:24 +0900

    Feat: Error handler 틀 구현

--- src/app.js
+++ src/app.js
@@ -12,6 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 // 1) 고객정보 및 주문내역정보 업로드 API
 app.use('/csv-file', upload.single('file'), insertCSV);
 
+// 2) 월별 매출 통계 API
+
+// 3) 주문 목록 조회 API
+
+app.use((err, req, res, next) => {
+  return res.status(400).json({
+    message: 'Failed',
+    err,
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log('connected');
 });
