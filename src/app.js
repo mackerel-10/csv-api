5f925a42d3d5fc7faee499655f9bf355f9558b1b
Sujin Jeon <mackerel0106@gmail.com>
2024-01-20 03:06:38 +0900

    Fix: use를 HTTP Method로 수정
    app.use로 잘못 선언되어 있던 부분을 HTTP Method에 맞춰 수정

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import express, { json } from 'express';
+import express from 'express';
 import dotenv from 'dotenv/config';
-import { insertCSV, salesStatistics } from './services';
 import multer from 'multer';
+import { insertCSV, salesStatistics, getOrders } from './services';
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -10,12 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // 1) 고객정보 및 주문내역정보 업로드 API
-app.use('/csv-file', upload.single('file'), insertCSV);
+app.post('/csv-file', upload.single('file'), insertCSV);
 
 // 2) 월별 매출 통계 API
-app.use('/sales-statistics', salesStatistics);
+app.get('/sales-statistics', salesStatistics);
 
 // 3) 주문 목록 조회 API
+app.get('/orders', getOrders);
 
 app.use((err, req, res, next) => {
   return res.status(400).json({
